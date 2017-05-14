177292167765fd577c262c906628b24687532e20
Mitchell Hentges <mitch9654@gmail.com>
2017-05-14 14:53:51 -0700

    Makes rsync not quiet

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 set -e
-rsync -r --delete-after --quiet -e "ssh -i /tmp/deploy-rsa" --include '*.html' --include '*.css' --include '*.png' . deploy@getfambit.com:/usr/share/nginx/www.getfambit.com
+rsync -r --delete-after -e "ssh -i /tmp/deploy-rsa" --include '*.html' --include '*.css' --include '*.png' . deploy@getfambit.com:/usr/share/nginx/www.getfambit.com
 ssh -i /tmp/deploy-rsa deploy@getfambit.com "chgrp -R http /usr/share/nginx/www.getfambit.com"
