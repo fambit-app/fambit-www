c25354d832b87b8325c5abd36858bab855f662a2
Mitchell Hentges <mitch9654@gmail.com>
2017-05-14 15:05:51 -0700

    Testing code change and deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 set -e
-echo "Is my script running"
 ls -al $TRAVIS_BUILD_DIR
 rsync -r --delete-after -e "ssh -i /tmp/deploy-rsa" --include '*.html' --include '*.css' --include '*.png' $TRAVIS_BUILD_DIR deploy@getfambit.com:/usr/share/nginx/www.getfambit.com
 ssh -i /tmp/deploy-rsa deploy@getfambit.com "chgrp -R http /usr/share/nginx/www.getfambit.com"
