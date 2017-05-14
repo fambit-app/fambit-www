d5a96ae1ba84b47e5629e42243d2db400cdfe848
Mitchell Hentges <mitch9654@gmail.com>
2017-05-14 15:13:50 -0700

    smh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 set -e
 ls -al $TRAVIS_BUILD_DIR
-rsync -rvv --delete-after -e "ssh -i /tmp/deploy-rsa" --include '*.html' --include '*.css' --include '*.png' --exclude '*' '$TRAVIS_BUILD_DIR/* deploy@getfambit.com:/usr/share/nginx/www.getfambit.com
+rsync -rvv --delete-after -e "ssh -i /tmp/deploy-rsa" --include '*.html' --include '*.css' --include '*.png' --exclude '*' '$TRAVIS_BUILD_DIR/*' deploy@getfambit.com:/usr/share/nginx/www.getfambit.com
 ssh -i /tmp/deploy-rsa deploy@getfambit.com "chgrp -R http /usr/share/nginx/www.getfambit.com"
