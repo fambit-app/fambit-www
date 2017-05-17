9ecc5f1199a5165afe360bc1a66e779e810f99df
Mitchell Hentges <mitch9654@gmail.com>
2017-05-16 22:53:31 -0700

    Deploy fonts, too

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 set -e
 ls -al $TRAVIS_BUILD_DIR
-rsync -r --delete -e "ssh -i /tmp/deploy-rsa" --include '*.html' --include '*.css' --include '*.png' --exclude '*' $TRAVIS_BUILD_DIR/* deploy@getfambit.com:/usr/share/nginx/www.getfambit.com
+rsync -r --delete -e "ssh -i /tmp/deploy-rsa" --include '*.html' --include '*.css' --include '*.png' --include '*.otf' --include '*.woff2' --exclude '*' $TRAVIS_BUILD_DIR/* deploy@getfambit.com:/usr/share/nginx/www.getfambit.com
 ssh -i /tmp/deploy-rsa deploy@getfambit.com "chgrp -R http /usr/share/nginx/www.getfambit.com && chmod -R 750 -R /usr/share/nginx/www.getfambit.com"
